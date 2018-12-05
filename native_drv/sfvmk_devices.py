10681240f9c9c109954ed3e4fdf042829097ecb5
Vijay Srivastava <vijays@solarflare.com>
2018-12-05 09:38:21 +0530

    Bug83721: Add sub device id and sub vendor id of supported NICs in sfvmk_devices.py
    hg-changeset: 9c1ce51d71b83d59d4a7fcf82a471b6e93dd091e

--- native_drv/sfvmk_devices.py
+++ native_drv/sfvmk_devices.py
@@ -9,11 +9,32 @@ DEV = {
             "devices" : [
                {
                   "device id"         : "0a03",
+                  "sub vendor id"     : "1924",
                   "description"       : "SFC9220 10/40G Ethernet Controller",
                },
                {
                   "device id"         : "0b03",
-                  "description"       : "SFC9250 10/25/40/50/100G Ethernet Controller",
+                  "sub vendor id"     : "1924",
+                  "sub device id"     : "8022",
+                  "description"       : "XtremeScale X2522 10G Network Adapter",
+               },
+               {
+                  "device id"         : "0b03",
+                  "sub vendor id"     : "1924",
+                  "sub device id"     : "8028",
+                  "description"       : "XtremeScale X2522 25G Network Adapter",
+               },
+               {
+                  "device id"         : "0b03",
+                  "sub vendor id"     : "1924",
+                  "sub device id"     : "8027",
+                  "description"       : "XtremeScale X2541 Network Adapter",
+               },
+               {
+                  "device id"         : "0b03",
+                  "sub vendor id"     : "1924",
+                  "sub device id"     : "802a",
+                  "description"       : "XtremeScale X2542 Network Adapter",
                },
 
             ],

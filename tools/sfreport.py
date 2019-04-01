1ffbddaa183d09e0a8838446fc4e1797b242dc17
Gautam Dawar <gdawar@solarflare.com>
2019-04-01 09:27:51 +0100

    Task 71858 : Update version numbers to v2.3.0.0008
    hg-changeset: cf1a2ac50e706e1cffb8c7b6d1a286ce82e4367d

--- tools/sfreport.py
+++ tools/sfreport.py
@@ -16,7 +16,7 @@ from collections import defaultdict
 
 # sfreport version to be incremented for any changes made before releases:
 # major minor build
-SFREPORT_VERSION = "2.3.0.0006"
+SFREPORT_VERSION = "2.3.0.0008"
 
 def terminate(process, timeout, cmd, mode):
     """ function to terminate a process """
